99e58fd4065152fa6a984f6ae0074be13a754d5a
iguc <iguti.yuu@gmail.com>
2020-06-29 15:10:09 +0900

    button

--- common.js
+++ common.js
@@ -1,5 +1,11 @@
+var presenters = ["Aさん", "Bさん", "Cさん", "Dさん", "Eさん", "Fさん"];
+
 $(function () {
 
     Sortable.create($('.list')[0]);
 
 });
+
+$('#double').on('click', function () {
+    $(this).css('pointer-events','none');
+  });
