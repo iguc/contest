94abffba5fd68f71edef797d50b36e7c0f946301
iguc <iguti.yuu@gmail.com>
2020-06-29 15:57:23 +0900

    cb

--- common.js
+++ common.js
@@ -10,3 +10,15 @@ $('#double').on('click', function () {
     $(this).css('pointer-events', 'none');
     $(this).css('readonly', true);
 });
+
+
+$('#check').change(function() {
+    // チェックが入っていたら有効化
+    if ( $(this).is(':checked') ){ 
+        // ボタンを有効化
+        $('#button').prop('disabled', false);
+    } else { 
+        // ボタンを無効化
+        $('#button').prop('disabled', true); 
+    }
+});
