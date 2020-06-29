d32778f4faee4c42b3c1ae7e38dc4a21ae4c8497
iguc <iguti.yuu@gmail.com>
2020-06-29 13:10:08 +0900

    js

--- common.js
+++ common.js
@@ -1,24 +1,4 @@
-let sortable = Sortable.create(list, {
-    group: "list",
-    animation: 100
+$(function () {
+    var sample1 = document.getElementById('sample1');
+    Sortable.create(sample1);
 });
-
-
-Sortable.create(hoge, {
-    group: "save",
-    store: {
-        get: function (sortable) {
-            var order = localStorage.getItem(sortable.options.group.name);
-            return order ? order.split('|') : [];
-        },
-        set: function (sortable) {
-            var order = sortable.toArray();
-            localStorage.setItem(sortable.options.group.name, order.join('|'));
-        }
-    }
-});
-
-
-
- var sample1 = document.getElementById('sample1');
- var sortable1 = Sortable.create(sample1);
