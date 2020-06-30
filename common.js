a5c08f4b5896bf2868fd0c7cc911f81384d9e320
iguc <iguti.yuu@gmail.com>
2020-06-30 13:30:18 +0900

    css js

--- common.js
+++ common.js
@@ -12,20 +12,30 @@ $(function () {
 // });
 
 
-$('#check').change(function() {
+$('#check').change(function () {
     // チェックが入っていたら有効化
-    if ( $(this).is(':checked') ){ 
+    if ($(this).is(':checked')) {
         // ボタンを有効化
         $('#button').prop('disabled', false);
-    } else { 
+    } else {
         // ボタンを無効化
-        $('#button').prop('disabled', true); 
+        $('#button').prop('disabled', true);
     }
 });
 
 
 
-  $("#check").on("click", function(){
+$("#check").on("click", function () {
     var v = $(this).val();
     sessionStorage.setItem('key', v);
-  });
+});
+
+$(function () {
+    $("check").click(function () {
+        if (this.checked) {
+            $("button").html("<button id='button' disabled='true' value='送信する' onclick='location.href='dest''>送信</button>");
+        } else {
+            $("button").html("送信");
+        }
+    });
+});
