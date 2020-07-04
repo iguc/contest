f2196561c982c3e1f493dffd7e6b7b8a86e4b105
iguc <iguti.yuu@gmail.com>
2020-07-05 01:12:14 +0900

    css

---
+++ main.js
@@ -0,0 +1,5 @@
+function sayHello(name) {
+    console.log("Hello " + name + "!"); // ES6のテンプレート文字列を使った
+}
+var myName = "TypeScript";
+sayHello(myName);
---
+++ main.ts
@@ -0,0 +1,5 @@
+function sayHello(name: string): void {
+    console.log(`Hello ${name}!`); // ES6のテンプレート文字列を使った
+  }
+  let myName: string = "TypeScript";
+  sayHello(myName);
