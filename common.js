fef9b6c61fb1399b86b1a39b7ab78282857ff829
iguc <iguti.yuu@gmail.com>
2020-07-05 02:48:18 +0900

    name

--- common.js
+++ common.js
@@ -12,30 +12,30 @@ $(function () {
 // });
 
 
-$('#check').change(function () {
-    // チェックが入っていたら有効化
-    if ($(this).is(':checked')) {
-        // ボタンを有効化
-        $('#button').prop('disabled', false);
-    } else {
-        // ボタンを無効化
-        $('#button').prop('disabled', true);
-    }
-});
+// $('#check').change(function () {
+//     // チェックが入っていたら有効化
+//     if ($(this).is(':checked')) {
+//         // ボタンを有効化
+//         $('#button').prop('disabled', false);
+//     } else {
+//         // ボタンを無効化
+//         $('#button').prop('disabled', true);
+//     }
+// });
 
 
 
-$("#check").on("click", function () {
-    var v = $(this).val();
-    sessionStorage.setItem('key', v);
-});
+// $("#check").on("click", function () {
+//     var v = $(this).val();
+//     sessionStorage.setItem('key', v);
+// });
 
-$(function () {
-    $("check").click(function () {
-        if (this.checked) {
-            $("button").html("<button id='button' disabled='true' value='送信する' onclick='location.href='dest''>送信する？</button>");
-        } else {
-            $("button").html("送信しない");
-        }
-    });
-});
+// $(function () {
+//     $("check").click(function () {
+//         if (this.checked) {
+//             $("button").html("<button id='button' disabled='true' value='送信する' onclick='location.href='dest''>送信する？</button>");
+//         } else {
+//             $("button").html("送信しない");
+//         }
+//     });
+// });
