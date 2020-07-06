c04478e11426b4a7ac8e1c603dda58f4cd328275
iguc <iguti.yuu@gmail.com>
2020-07-06 13:05:05 +0900

    d

--- common.js
+++ common.js
@@ -38,32 +38,7 @@ for (var i = 0; i < classname.length; i++) {
   classname[i].addEventListener("click", animateButton, false);  
 };
 
-function put_result(ui) {
-  $("#result_box").html("現在の要素のIDの順番は下記のとおりです。<br />");
 
-  $("#box").find(".card").each(function (index, element) {
-    var id = $(this).attr("id");
-    $("#result_box").append(id + "<br />");
-  });
-
-  if (ui) { // もしドラッグ後であれば
-    // ドラッグされた要素のIDを取得
-    var dragged_id = ui.item[0].id;
-    $("#result_box").append("ドラッグされた要素のIDは" + dragged_id + "です<br />");
-  }
-}
-$(function () {
-  put_result();
-  $("#box").sortable({
-    placeholder: "ui-state-highlight",
-    axis: "y", // ドラッグの方向を縦に固定
-    "opacity": 0.5, // ドラッグ中の透明度
-    "update": function (event, ui) { // ドラッグ完了後のコールバック
-      put_result(ui);
-    }
-  });
-
-});
 
 // $('#datas').sortable({
 //   update: function(){
