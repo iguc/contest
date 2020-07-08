3bbd5f9c15ec5169fdfa78c87c4db3ece8ec6330
iguc <iguti.yuu@gmail.com>
2020-07-08 11:21:30 +0900

    a

--- common.js
+++ common.js
@@ -1,17 +1,3 @@
-// $(function () {
-//   Sortable.create($(".list")[0]);
-// });
-// Sortable.create($(".list")[0], {
-//   animation: 110, // ミリ秒で指定
-// });
-
-// $(function($) {
-//   $('.box').on('click', function() {
-//     $('.box').css('background', '#edb54c');
-//   });
-// });
-
-
 $(function ($) {
   $('#rater').change(function () {
     $('.button').css('background', '#3CB371');
@@ -43,7 +29,6 @@ var classname = document.getElementsByClassName("button");
 for (var i = 0; i < classname.length; i++) {
   classname[i].addEventListener("click", animateButton, false);
 };
-
 $(function () {
   put_result();
   $("#box").sortable({
@@ -56,27 +41,17 @@ $(function () {
   });
 
 });
+function put_result(ui) {
+  $("#result_box").html("現在の要素のIDの順番は下記のとおりです。<br />");
 
+  $("#box").find(".card").each(function (index, element) {
+    var id = $(this).attr("id");
+    $("#result_box").append(id + "<br />");
+  });
 
-
-  function put_result(ui) {
-    $("#result_box").html("現在の要素のIDの順番は下記のとおりです。<br />");
-
-    $("#box").find(".card").each(function (index, element) {
-      var id = $(this).attr("id");
-      $("#result_box").append(id + "<br />");
-    });
-
-    if (ui) { // もしドラッグ後であれば
-      // ドラッグされた要素のIDを取得
-      var dragged_id = ui.item[0].id;
-      $("#result_box").append("ドラッグされた要素のIDは" + dragged_id + "です<br />");
-    }
+  if (ui) { // もしドラッグ後であれば
+    // ドラッグされた要素のIDを取得
+    var dragged_id = ui.item[0].id;
+    $("#result_box").append("ドラッグされた要素のIDは" + dragged_id + "です<br />");
   }
-
-// $('#datas').sortable({
-//   update: function(){
-//       var log = $(this).sortable("toArray");
-//       $("#log").text(log);
-//   }
-// });
+}
