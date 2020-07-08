9018fe12a89c20eef020d168aa468aa3fd57215a
iguc <iguti.yuu@gmail.com>
2020-07-08 10:43:05 +0900

    a

--- common.js
+++ common.js
@@ -27,7 +27,9 @@ $(function ($) {
     $('.button').css('pointer-events', 'none');
 
     var request = new XMLHttpRequest();
-    request.open("GET", 'https://9ai1mdy8fi.execute-api.ap-northeast-1.amazonaws.com/db?foo=13456qweasd/', true);
+    request.open("GET", 'https://9ai1mdy8fi.execute-api.ap-northeast-1.amazonaws.com/db?rater='
+      + null + '&iok=' + null + '&kmt=' + null + '&nki=' + null + '&nsm=' + null + '&fji=' + null + '&msm=' + null
+    , true);
     request.send();
 
    //window.location.href = 'https://9ai1mdy8fi.execute-api.ap-northeast-1.amazonaws.com/db?foo=13456qweasd/';
